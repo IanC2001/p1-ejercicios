dae5e1030124cf31b367f553af5fe830a18ba802
ian <iancane001@gmail.com>
2024-08-13 23:06:57 -0300

    agrege estilos, y agrege etiqueta inicio

--- 2024-02-clase-01-ej-01/index.js
+++ 2024-02-clase-01-ej-01/index.js
@@ -1,28 +1,25 @@
-//Pido dos extremos para comprar
+// Pido dos extremos para comprar
+const max = parseInt(prompt("Ingrese el máximo para comprar"));
+const min = parseInt(prompt("Ingrese el mínimo para comprar"));
 
-const max = parseInt(prompt("Ingrese el maximo para comprar"));
-const min = parseInt(prompt("Ingrese el minimo para comprar"));
+// Pido el número para comparar
+const num = parseInt(prompt("Ingrese el número a comparar"));
 
-//Pido el numero para comparar
-
-const num = parseInt(prompt("Ingrese el numero a comparar"));
-
-//Verifico si el numero esta en rango
-
-if(num <= min && num >= max){
-    //Entro aca solo si esta entre el max y el min
-    alert("el num esta en rango");
-}else{
-    alert("el num NO esta en rango");
+// Verifico si el número está en rango
+if (num >= min && num <= max) {
+    // Entro acá solo si está entre el min y el max (inclusive)
+    alert("El número está en rango");
+} else {
+    alert("El número NO está en rango");
 }
-//Pregunto si es par o inpar
 
-if(num % 2 == 1){
-    //Es impar porque tiene resto 1
-    alert("El numero es impar");
-}else{
-    //Es par porque tiene resto 0
-    alert("El numero es par");
+// Pregunto si es par o impar
+if (num % 2 === 0) {
+    // Es par porque tiene resto 0
+    alert("El número es par");
+} else {
+    // Es impar porque tiene resto 1
+    alert("El número es impar");
 }
 
 
