88c02f838e1c87b637c046b6e87b8747de7f0244
ian <iancane001@gmail.com>
2024-09-15 22:16:13 -0300

    funciones para mostrar datos

--- 2024-02-clase-04-ej-02/clicks.js
+++ 2024-02-clase-04-ej-02/clicks.js
@@ -1,11 +1,57 @@
-function cargarAlumno(){
-
+function cargarAlumnos(){
+//Pido el nombre del alumno
+alumno = validarString("Ingrese el nombre del alumno");
 }
 
-function cargarDatos(){
-    
+function cargarMateria() {
+    // Pido la materia y valido
+    let materia = validarString("Ingrese el nombre de la materia");
+    // Guardo la materia en su array
+    materias.push(materia);
+    // Pido nota y valido
+    let nota = validarNota(`Ingrese la nota de final de ${materia}`);
+    // Guardo la nota en su array
+    notas.push(nota);
 }
 
-function mostrar (){
-    
+function mostrar(){
+    /**
+     * <h1>${alumno}</h1>
+     * <h2>Materias</h2>
+     * <ul>
+     *      <li>${materia} - ${notas}</li>
+     *      <li>${materia} - ${notas}</li>
+     *      <li>${materia} - ${notas}</li>
+     *      <li>${materia} - ${notas}</li>
+     * </ul>
+     */
+
+    //Encuentro el container
+    const container = document.querySelector(".container");
+    //Limpio el container 
+    container.innerHTML += "";
+
+    //Creo el h1 con el nombre del alumno
+    container.innerHTML += `<h1>${alumno}</h1>`;
+    container.innerHTML += `<h2>Materias</h2>`;
+    //Creo la lista(se cierra automáticamente)
+    container.innerHTML += `<ul>`;
+    //Encuentro la lista que generé recién
+    const ul = document.querySelector("ul")
+    //Agrego las materias
+
+    //Con un for normal
+    /* for(let i = 0; i < materias.length; i++){
+        ul.innerHTML += `<li>${materias[i]} - ${notas[i]}</li>`;
+    } */
+
+    //Con un for in (recorre el array de principio a fin)
+     for(let i in materias){
+        ul.innerHTML += `<li>${materias[i]} - ${notas[i]}</li>`;
+    } 
+
+    //Con un for of (a medias anda, toma los elementos del array, no a los indices)
+    /*  for(let materia of materias){
+        ul.innerHTML += `<li>${materia[i]}</li>`;
+    } */
 }
--- 2024-02-clase-04-ej-02/index.js
+++ 2024-02-clase-04-ej-02/index.js
@@ -1,15 +1,7 @@
+//Variable para guardar el nombre de alumno
+let alumno = "Fulano de tal";
 //Variable para almacenar los datos de las materias
 let materias = [], notas = [];
 
-//Pido el nombre del alumno
-const alumno = validarString("Ingrese el nombre del alumno");
 
-do{
-    //Pido la materia y valido
-    let materia = validarString("Ingrese el nombre de la materia");
-    //Pido la nota y valido
-    let nota = validarNota(`Ingrese la nota final de ${materia}`);
-    //Guardo la nota en su array
-    notas.push(nota);
-}while(confirm("Otra materia y nota para cargar?"));
 
