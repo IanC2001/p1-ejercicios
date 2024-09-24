beb82ebb349b7812e7a1e3c5edbe28cc321e48c7
ian <iancane001@gmail.com>
2024-09-24 10:36:51 -0300

    .this

--- 2024-02-clase-05-ej-03/clicks.js
+++ 2024-02-clase-05-ej-03/clicks.js
@@ -18,6 +18,27 @@ function cargarMateria(){
     alumno.materias.push(materia);
 }
 function cargarAlumno(){
+    //Agrego una funcion como propiedad
+    alumno.toHTML = function (){
+        //Variable de string vacio
+        let str = "";
+        for(let alumno of alumnos){
+            //Agrego h1 con nombre del alumno
+            str +=`<h1>${this.nombre}</h1>\n`;
+            str +=`<h2>Materias</h2>\n`;
+            //Creo un string auxiliar
+            str = `<ul>`;
+            //Recorro las materias del alumno
+            for(let materia of this.materias){
+                //Armo el li
+                str += `<li> ${materia.nombre} - ${materia.nota}</li>\n` 
+            }
+            //Cierro el ul
+            str += `</ul>`;
+            //Devuelvo str
+            return str;
+        }
+    }
     //Subo al alumno a la lista
     alumnos.push(alumno);
     //Inicializar un objeto nuevo
@@ -27,5 +48,12 @@ function cargarAlumno(){
 }
 
 function mostrarLista(){
-    
+    //Traigo el primer elemento que tenga la clase  container y lo guardo en una variable
+    const div = document.querySelector(".container");
+    //Limpio el html
+    div.innerHTML = "";
+    for(let alumno of alumnos){
+        //Le paso al div el html que genera el propio objeto
+        div.innerHTML += alumno.toHTML();
+    }
 }
--- 2024-02-clase-05-ej-03/validaciones.js
+++ 2024-02-clase-05-ej-03/validaciones.js
@@ -1,39 +1,46 @@
 
-function validarString(){
-    let str, datoValido = true;
-    do{
-        str = prompt(msg);
+function validarString(msg) {
 
-        if(str === null || str.trim() === ""){
+    let str, datoValido = true;
+    do {
+        str = prompt(msg);
+        // Valido que no hayan apretado cancel o dejado vacio
+        if(str === null || str.trim() === "") {
             alert("Complete el campo");
             datoValido = false;
-        }else if(!isNaN()){
-            alert("Ingrese texto, no numeros");
+        }
+        else if(!isNaN(str)) {
+            alert("No debe ser números");
             datoValido = false;
-        }else{
+        }
+        else {
             datoValido = true;
         }
-    }while(!datoValido);
-    //El dato es OK
+
+    } while(!datoValido);
+    // El dato es OK
     return str;
 }
 
-function validarNota(){
-    let nota, datoValido;
+function validarNota() {
 
-    do{
+    let nota, datoValido;
+    do {
         nota = parseInt(prompt("Ingrese la nota de aprobado"));
 
-        if(isNaN(nota)){
+        if(isNaN(nota)) {
             alert("La nota debe ser numérica");
             datoValido = false;
-        }else if(nota < 4 || nota > 10){
+        }
+        else if(nota < 4 || nota > 10) {
             alert("Se aprueba entre 4 y 10");
             datoValido = false;
-        }else{
+        }
+        else {
             datoValido = true;
         }
-    }while(!datoValido);
+
+    } while(!datoValido);
 
     return nota;
 }
